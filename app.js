b5101a9d25b6a782ed22ee96d5977cb19c033e45
salohiddindev1 <169024491+salohiddindev1@users.noreply.github.com>
2024-06-19 21:12:03 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+let random = Math.floor(Math.random() * 100);
+
+alert("sizda xozir random sonlar chiqishni boshlaydi");
+
+alert("random orqali chiqqan son: " + random);
+
+alert("biz random sonni kopytirmoqchi va bolmoqchimiz");
+
+alert(
+  `sizda chiqqan son${random}\n va biz uni 2ga kopaytirib ${
+    random * 2
+  } sonni chiqazdik: \n va shu sonni 2ga bolib shu sonni chiqazdik: ${
+    random / 2
+  }`
+);
